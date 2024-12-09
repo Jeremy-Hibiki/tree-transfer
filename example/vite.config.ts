37b09ec9b4098a25e4732ec57c644a5108327350
Jeremy-Hibiki <JeremyJiang430@outlook.com>
2024-12-10 00:26:11 +0800

    fix: example setup

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,7 +1,13 @@
 import react from '@vitejs/plugin-react';
+import { resolve } from 'node:path';
 import UnoCSS from 'unocss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [UnoCSS(), react()],
+  resolve: {
+    alias: {
+      '@jeremy-hibiki/tree-transfer': resolve(__dirname, '../tree-transfer/src'),
+    },
+  },
 });
